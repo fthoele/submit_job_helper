2577eba77c16441a172b7c738632a6450c0fb3aa
Florian Thoele <florian.thoele@gmail.com>
2018-11-27 16:39:12 +0100

    OOP version

--- submit_job.py
+++ submit_job.py
@@ -7,56 +7,139 @@ import subprocess
 import sys
 from time import localtime, strftime
 
-subcmd = "bsub"
+class JobRunner:
+    def __init__(self, filename, logfile):
+        self.filename = filename
+        self.logfile = logfile
 
-def write_to_log(logfile, fn, jobname, outlines):
-    tstr = strftime("%Y-%m-%d %H:%M:%S", localtime())
-    jobid = outlines[0].split()[1].replace("<", "").replace(">", "")
-    cwd = os.getcwd()
-    cwd = cwd.replace('/cluster/home/thoelef', '~')
-    cwd = cwd.replace('/cluster/work/beta3/thoelef', '~/escr')
-   
-    logfile.write("{0}\t{1}\t{2}\t{3}\t{4}\n".format(tstr, jobid, cwd, fn, jobname))
     
+    def run(self):
+        try:
+            output = self.submit()
+            jobinfo = self.get_jobinfo(output)
+            self.write_to_log(jobinfo)
+        except IOError as e:            
+            print(e)
+            
+    
+    def submit(self):
+        raise NotImplementedError()
 
-def get_euler_jobname(filename):
-    jobname = filename
-    with open(filename) as infile:
-        lines = infile.readlines()        
-        for l in lines:
-            splits = l.strip().split()
-            if len(splits) < 2:
-                continue
-            if splits[0] == "#BSUB" and splits[1] == "-J":
-                jobname = splits[2]
-    return jobname
+
+    def get_jobinfo(self, output):
+        raise NotImplementedError()
+
+    
+    def write_to_log(self, jobinfo):
+        tstr = strftime("%Y-%m-%d %H:%M:%S", localtime())
+            
+        self.logfile.write(u"{0}\t{1}\t{2}\t{3}\t{4}\n".format(
+            tstr, 
+            jobinfo.get('jobid', 0), 
+            jobinfo.get('cwd', "NONE"),
+            self.filename, 
+            jobinfo.get('jobname', self.filename)
+        ))
 
 
-def submit_job(infile_filename):
-    with open(infile_filename) as infile:
-        outp = subprocess.check_output(subcmd, stdin=infile).decode("utf-8")
+class EulerJobRunner(JobRunner):
+    def submit(self):
+        subcmd = "bsub"
+        with open(self.filename) as infile:
+            outp = subprocess.check_output(subcmd, stdin=infile)
+            if sys.version_info[0] == 3:
+                outp = outp.decode("utf-8")
+            outlines = outp.split('\n')
+            print(outlines[0])
+        return outlines
+
+    
+    def get_jobinfo(self, output):
+        jobinfo = {'jobname': self.filename}
+
+        with open(self.filename) as infile:
+            lines = infile.readlines()        
+            for l in lines:
+                splits = l.strip().split()
+                if len(splits) < 2:
+                    continue
+                if splits[0] == "#BSUB" and splits[1] == "-J":
+                    jobinfo['jobname'] = splits[2]
+
+        jobinfo['jobid'] = output[0].split()[1].replace("<", "").replace(">", "")
+        cwd = os.getcwd()
+        cwd = cwd.replace('/cluster/home/thoelef', '~')
+        cwd = cwd.replace('/cluster/work/beta3/thoelef', '~/escr')
+        jobinfo['cwd'] = cwd
+
+        return jobinfo
+
+
+class DaintJobRunner(JobRunner):
+    def submit(self):
+        subcmd = "sbatch"
+        outp = subprocess.check_output([subcmd, self.filename])
+        if sys.version_info[0] == 3:
+            outp = outp.decode("utf-8")
         outlines = outp.split('\n')
         print(outlines[0])
-    return outlines
+        return outlines
 
 
-def process_file(fn, logfile):
-    if not os.path.isfile(fn):
-        print("Invalid filename: ", fn)
-        return
-    
-    outlines = submit_job(fn)
-    jobname = get_euler_jobname(fn)
-    write_to_log(logfile, fn, jobname, outlines)
+    def get_jobinfo(self, output):
+        jobinfo = {'jobname': self.filename}
+
+        with open(self.filename) as infile:
+            lines = infile.readlines()     
+
+            for l in lines:
+                splits = l.strip().split()
+                if len(splits) < 2:
+                    continue
+                if splits[0] == "#SBATCH" and splits[1].startswith("--job-name"):
+                    jobname = splits[1].split('=')[1].replace('"', '')
+                    jobinfo['jobname'] = jobname
+
+        jobinfo["jobid"] = output[0].split()[3]
+        cwd = os.getcwd()
+        cwd = cwd.replace('/users/thoelef', '~')
+        cwd = cwd.replace('/mnt/lnec/fthoele', '~/mscr')
+        cwd = cwd.replace('/scratch/rosa/fthoele', '~/rscr')
+        jobinfo['cwd'] = cwd
+
+        return jobinfo
 
 
-def run(logfile):
+def get_cluster(hostname=None):
+    homedir = os.getenv("HOME")
+    clusters = {
+        "daint": {"logfile": os.path.join(homedir, "log-daint.txt"),
+                "runner": DaintJobRunner},
+        "euler": {"logfile": os.path.join(homedir, "log-euler.txt"),
+                "runner": EulerJobRunner},        
+    }
+
+    if not hostname:
+        hostname = os.getenv("HOSTNAME")
+
+    if hostname.startswith("eu"):
+        return clusters['euler']
+    elif "daint" in hostname:
+        return clusters['daint']
+    else:
+        raise NotImplementedError("Unknown cluster: {}".format(hostname))
+
+
+def run(runner_class, logfile):
     filenames = sys.argv[1:]
     for fn in filenames:
-        process_file(fn, logfile)
+        runner = runner_class(fn, logfile)
+        runner.run()
 
 
 if __name__ == "__main__":
-    logfile_fn = "/cluster/home/thoelef/log-euler.txt"
-    with open(logfile_fn, "a", encoding="utf-8") as log:
-        run(log)
+    cluster = get_cluster()
+    
+    from io import open
+    with open(cluster['logfile'], "a", encoding="utf-8") as logfile:
+        run(cluster['runner'], logfile)
--- test_submit_job.py
+++ test_submit_job.py
@@ -6,6 +6,9 @@ import submit_job
 def check_output_mock(*args, **kwargs):
     return b"Job <1234> is submitted to queue <normal.120h>."
 
+def check_output_daint_mock(*args, **kwargs):
+    return b"Submitted batch job 1234"
+
 @pytest.fixture
 def setup(mocker):
     args = ["", "jobscript"]
@@ -15,7 +18,7 @@ def setup(mocker):
 
 def test_run(setup, capsys):
     with tempfile.TemporaryFile(mode="a+") as fp:
-        submit_job.run(fp)
+        submit_job.run(submit_job.EulerJobRunner, fp)
 
         fp.seek(0)
         result = fp.read()
@@ -34,7 +37,7 @@ def test_run_2(mocker):
     mocker.patch("subprocess.check_output", check_output_mock)
 
     with tempfile.TemporaryFile(mode="a+") as fp:
-        submit_job.run(fp)
+        submit_job.run(submit_job.EulerJobRunner, fp)
 
         fp.seek(0)
         lines = fp.readlines()
@@ -56,6 +59,25 @@ def test_invalid_filename(mocker, capsys):
     mocker.patch("subprocess.check_output", check_output_mock)
 
     with tempfile.TemporaryFile(mode="a+") as fp:
-        submit_job.run(fp)
+        submit_job.run(submit_job.EulerJobRunner, fp)
         captured = capsys.readouterr()
-        assert captured.out == "Invalid filename:  invalid_filename\n"
+        assert captured.out == "[Errno 2] No such file or directory: 'invalid_filename'\n"
+
+
+
+def test_daint(mocker, capsys):
+    args = ["", "jobscript_daint"]
+    mocker.patch("sys.argv", args)
+    mocker.patch("subprocess.check_output", check_output_daint_mock)
+
+    with tempfile.TemporaryFile(mode="a+") as fp:
+        submit_job.run(submit_job.DaintJobRunner, fp)
+        captured = capsys.readouterr()
+        assert captured.out == "Submitted batch job 1234\n"
+
+        fp.seek(0)
+        result = fp.read()
+        words = result.split()
+        assert words[2] == "1234"
+        assert words[4] == "jobscript_daint"
+        assert words[5] == "jobname"
